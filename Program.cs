383795fcb9a6a8d39ca09dd9c169a6379227823e
arlihakan81 <155262471+arlihakan81@users.noreply.github.com>
2024-02-14 20:30:29 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,138 +5,143 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Hesap_Makinesi_Konsol
+namespace Otopark_Otomasyonu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<decimal> sayilar = new List<decimal>();
-
-
+            /*
+              Bir otoparkta ticari aracın saatlik ücreti 15 TL, Otobüsün saatlik ücreti 25 TL,
+            Minibüsün saatlik ücreti 20, Kamyonetin saatlik 30 TL, Binek aracın saatlik ücreti 10 TL
+            şeklinde ücretlendirme vardır. 
+                Ticari aracın 1 saat aşımında her saat başına %12 artış,
+                Minibüsün 1 saat sonrasında her saat başına %15 artış,
+                Otobüsün 1 saat sonrasında her saat başına %18 artış,
+                Kamyonetin 1 saat sonrasında her saat başına %20 artış,
+                Binek aracın 1 saat sonrasında her saat başına %10 artış yapılacaktır.
+            Buna göre, kullanıcıdan araç tipi ve park süresi girilmesi istenecektir. Ödenecek tutar belirlenip
+            ekrana yazdırılacaktır.
+            */
 
             try
             {
-                Console.WriteLine("****Hesap Makinesi****");
-                Thread.Sleep(1000);
-                Console.WriteLine("1 --> Toplama");
-                Console.WriteLine("2 --> Çıkarma");
-                Console.WriteLine("3 --> Çarpma");
-                Console.WriteLine("4 --> Bölme");
-                Console.WriteLine("5 --> Üs Alma");
-                Console.WriteLine("6 --> Faktöriyel");
-                IslemSec:
-                Console.WriteLine("Lütfen yukarıdaki işlemlerden birini seçiniz");
-                int islem = int.Parse(Console.ReadLine());
-                if(islem > 0 && islem < 7)
+                double ticariArac = 15, otobus = 25, minibus = 20, binekArac = 10, kamyonet = 30;
+                double odenecekTutar = 0;
+                BasaDon:
+                Console.WriteLine("********Otoparka Hoşgeldiniz**********");
+                Thread.Sleep(3000);
+                Console.WriteLine("Binek araç için -->  1");
+                Console.WriteLine("Otobüs için --> 2");
+                Console.WriteLine("Ticari araç için --> 3");
+                Console.WriteLine("Minibüs için --> 4");
+                Console.WriteLine("Kamyonet için --> 5");
+                Console.WriteLine("Lütfen bir araç tipi seçin");
+                int secim = int.Parse(Console.ReadLine());
+                if(secim > 0 && secim < 6)
                 {
-                    if(islem == 1)
+                    if(secim == 1)
                     {
-                        Console.WriteLine("Toplama işlemi siz 0 girene kadar girdiğiniz tüm sayıları toplar.");
-                        Thread.Sleep(2000);
-                        decimal i = 0;
-                        do
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Lütfen aracınızın park süresini girin");
+                        int parkSuresi = int.Parse(Console.ReadLine());
+                        if(parkSuresi > 1)
                         {
-                            Console.WriteLine("Lütfen bir sayı giriniz");
-                            decimal sayi = decimal.Parse(Console.ReadLine());
-                            sayilar.Add(sayi);
-                        } while (sayilar.Contains(i));
-                        Topla(sayilar);
-                    }
-                    else if (islem == 2)
-                    {
-                        Console.WriteLine("Çıkarma işlemi siz 0 girene kadar girdiğiniz tüm sayıları toplar.");
-                        Thread.Sleep(2000);
-                        decimal i = 0;
-                        do
+                            for(int i = 1; i <= parkSuresi; i++)
+                            {
+                                odenecekTutar += binekArac + binekArac * 0.1;
+                            }
+                        }
+                        else if(parkSuresi <= 1)
                         {
-                            Console.WriteLine("Lütfen bir sayı giriniz");
-                            decimal sayi = decimal.Parse(Console.ReadLine());
-                            sayilar.Add(sayi);
-                        } while (sayilar.Contains(i));
-                        Cikar(sayilar);
+                            odenecekTutar += binekArac;
+                        }
                     }
-                    else if (islem == 3)
+                    else if (secim == 2)
                     {
-                        Console.WriteLine("Çarpma işlemi siz 0 girene kadar girdiğiniz tüm sayıları toplar.");
-                        Thread.Sleep(2000);
-                        decimal i = 0;
-                        do
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Lütfen aracınızın park süresini girin");
+                        int parkSuresi = int.Parse(Console.ReadLine());
+                        if (parkSuresi > 1)
                         {
-                            Console.WriteLine("Lütfen bir sayı giriniz");
-                            decimal sayi = decimal.Parse(Console.ReadLine());
-                            sayilar.Add(sayi);
-                        } while (sayilar.Contains(i));
-                        Carp(sayilar);
+                            for (int i = 1; i <= parkSuresi; i++)
+                            {
+                                odenecekTutar += otobus + otobus * 0.18;
+                            }
+                        }
+                        else if (parkSuresi <= 1)
+                        {
+                            odenecekTutar += otobus;
+                        }
                     }
-                    else if (islem == 4)
+                    else if (secim == 3)
                     {
-                        Console.WriteLine("Lütfen Bölüneni giriniz");
-                        int bolunen = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Lütfen Böleni giriniz");
-                        int bolen = int.Parse(Console.ReadLine());
-                        Bol(bolunen, bolen);
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Lütfen aracınızın park süresini girin");
+                        int parkSuresi = int.Parse(Console.ReadLine());
+                        if (parkSuresi > 1)
+                        {
+                            for (int i = 1; i <= parkSuresi; i++)
+                            {
+                                odenecekTutar += ticariArac + ticariArac * 0.12;
+                            }
+                        }
+                        else if (parkSuresi <= 1)
+                        {
+                            odenecekTutar += ticariArac;
+                        }
                     }
+                    else if (secim == 4)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Lütfen aracınızın park süresini girin");
+                        int parkSuresi = int.Parse(Console.ReadLine());
+                        if (parkSuresi > 1)
+                        {
+                            for (int i = 1; i <= parkSuresi; i++)
+                            {
+                                odenecekTutar += minibus + minibus * 0.18;
+                            }
+                        }
+                        else if (parkSuresi <= 1)
+                        {
+                            odenecekTutar += minibus;
+                        }
+                    }
+                    else if (secim == 5)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Lütfen aracınızın park süresini girin");
+                        int parkSuresi = int.Parse(Console.ReadLine());
+                        if (parkSuresi > 1)
+                        {
+                            for (int i = 1; i <= parkSuresi; i++)
+                            {
+                                odenecekTutar += kamyonet + kamyonet * 0.2;
+                            }
+                        }
+                        else if (parkSuresi <= 1)
+                        {
+                            odenecekTutar += kamyonet;
+                        }
+                    }
+
+                    Console.WriteLine("Ödenecek tutar hesaplanıyor....");
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Ödenecek tutarınız: {odenecekTutar}");
                 }
                 else
                 {
-                    Console.WriteLine("Lütfen işleminiz 1-6 arasında olsun");
-                    goto IslemSec;
+                    Console.WriteLine("Lütfen geçerli bir araç tipi seçin");
+                    goto BasaDon;
                 }
-
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
 
-
             Console.ReadLine();
-
-        }
-
-        static void Topla(List<decimal> sayilar)
-        {
-            decimal toplam = 0;
-            foreach(var sayi in sayilar)
-            {
-                toplam += sayi;
-                Console.WriteLine($"{toplam}");
-            }
-        }
-
-        static void Cikar(List<decimal> sayilar)
-        {
-            decimal fark = 0;
-            foreach (var sayi in sayilar)
-            {
-                fark -= sayi;
-                Console.WriteLine($"{fark}");
-            }
-        }
-
-        static void Carp(List<decimal> sayilar)
-        {
-            decimal carpim = 1;
-            foreach (var sayi in sayilar)
-            {
-                carpim *= sayi;
-                Console.WriteLine($"{carpim}");
-            }
-        }
-
-        static void Bol(int sayi1, int sayi2)
-        {
-            if(sayi2 != 0)
-            {
-                decimal bolum = 0;
-                bolum = sayi1 / sayi2;
-                Console.WriteLine($"{bolum}");
-            }
-            else
-            {
-                Console.WriteLine("Sıfıra bölme hatası");
-            }
         }
     }
 }
